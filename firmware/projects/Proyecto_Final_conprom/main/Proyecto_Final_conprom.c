4381f8c429d4ed0ab1f179c7bf403d16a61b932e
NachoGalluccio <gnachoperez@gmail.com>
2024-06-12 11:49:27 -0300

    Se actualiza .m proyecto final

--- firmware/projects/Proyecto_Final_conprom/main/Proyecto_Final_conprom.c
+++ firmware/projects/Proyecto_Final_conprom/main/Proyecto_Final_conprom.c
@@ -34,16 +34,16 @@
  * @brief baud rate
 */
 #define BAUD_RATE 115200
-#define CONFIG_PERIOD_MS 500
+
 /** @def DECIMAL
  * @brief Constante 10
 */
 #define DECIMAL 10
 
-/** @def CANTIDAD_MAXIMOS
- * @brief Cuantos maximos(picos) se quieren tomar en cuenta para el calculo de UMBRAL
+/** @def ULTIMOS_VALORES
+ * @brief Cuantos valores se toman en cuenta para suavizar la señal
 */
-#define CANTIDAD_MAXIMOS 10
+#define ULTIMOS_VALORES 10
 
 /** @def LECTURAS_CALIBRACION
  * @brief Cuantos valores se quieren tomar en cuenta
@@ -53,9 +53,15 @@
 /** @def FREC_LECTURA_US
  * @brief frecuencia de lectura de datos en us
 */
-#define FREC_LECTURA_US 10000
+#define FREC_LECTURA_US 20000
+
+/** @def PERIODO_REFRACTARIO_US
+ * @brief Tiempo por el cual no se va a tomar una nueva lectura
+*/
+#define PERIODO_REFRACTARIO_US 400000
 
 #define CONFIG_BLINK_PERIOD 500
+
 #define LED_BT	LED_1
 
 /*==================[internal data definition]===============================*/
@@ -69,7 +75,7 @@ int cantidad_pasos = 0;
 /** @def umbral
  * @brief Define el valor del umbral para medir los pasos
 */
-float umbral = 0.5;
+float umbral = 0.3;
 
 /** @def x_promedio
  * @brief Define el promedio de los valores leidos en X en el acelerometro en etapa de calibracion
@@ -86,31 +92,18 @@ float y_promedio = 0;
 */
 float z_promedio = 0;
 
-/** @def CONTAR_PASOS
- * @brief Bandera para determinar si contar o no un paso
-*/
-bool CONTAR_PASOS = true;
-
 /*==================[internal functions declaration]=========================*/
 
 /** @fn readacc_timer
  *  @brief Función invocada en la interrupción del timer A
- * 	@param[in] No hay parametros
- * 	@return
  */
 void readacc_timer(void *param)
 {
 	xTaskNotifyGive(readacc_task_handle); /* Envía una notificación a la tarea asociada */
 }
 
-void read_data(uint8_t * data, uint8_t length){
-    /* Data */
-}
-
 /** @fn control_prom_timer
  *  @brief Función invocada en la interrupción del timer B
- * 	@param[in] No hay parametros
- * 	@return
  */
 void control_prom_timer(void *param)
 {
@@ -119,17 +112,19 @@ void control_prom_timer(void *param)
 
 /** @fn readacc_task
  * 	@brief Tarea que controla el evento de leer el acelerometro
- * 	@param[in] No hay parametros
- * 	@return
 */
 void readacc_task(void *pvParameter)
 {
-    float maximos[CANTIDAD_MAXIMOS] = {0};
     float vector_acc = 0;
 
-    float ultimos_vectores_acc[20] = {0};
-    float promedio_ultimos_vectores_acc = 0;
+    uint16_t t_refractario = 0;
 
+    bool contar_pasos = true;
+    bool tiempo_muerto = false;
+
+    float ultimos_vectores_acc[ULTIMOS_VALORES] = {0};
+    float promedio_ultimos_vectores_acc = 0;
+    
     uint8_t contador = 0;
 
     float lectura_x, lectura_y, lectura_z;
@@ -146,39 +141,57 @@ void readacc_task(void *pvParameter)
         lectura_y = ReadYValue();
         lectura_z = ReadZValue();
 
-        promedio_ultimos_vectores_acc = promedio_ultimos_vectores_acc*20 - ultimos_vectores_acc[contador];
+        // Calcula el vector_acc
+        // Saco del promedio el valor que voy a sobre escribir
+        promedio_ultimos_vectores_acc = promedio_ultimos_vectores_acc*ULTIMOS_VALORES - ultimos_vectores_acc[contador];
 
+        // Sobreescribo el ultimo valor leido
         ultimos_vectores_acc[contador] = sqrt((lectura_x-x_promedio)*(lectura_x-x_promedio) + (lectura_y-y_promedio)*(lectura_y-y_promedio) + (lectura_z-z_promedio)*(lectura_z-z_promedio));
         
-        promedio_ultimos_vectores_acc = (promedio_ultimos_vectores_acc + ultimos_vectores_acc[contador])/20;
+        //Calculo el promedio
+        promedio_ultimos_vectores_acc = (promedio_ultimos_vectores_acc + ultimos_vectores_acc[contador])/ULTIMOS_VALORES;
 
         vector_acc = promedio_ultimos_vectores_acc;
 
-        if(vector_acc > umbral && CONTAR_PASOS){
-            CONTAR_PASOS = !CONTAR_PASOS; //Levanta bandera para no seguir contando pasos
-            cantidad_pasos++; //Suma un paso al detectar que el vector_acc supero el umbral y la bandera esta baja
+        if(tiempo_muerto){//Tiempo en que no se toma lectura
+            t_refractario = t_refractario + FREC_LECTURA_US/1000;
+            if (t_refractario >= PERIODO_REFRACTARIO_US/1000){
+                t_refractario = 0;
+                contar_pasos = !contar_pasos;
+                tiempo_muerto = !tiempo_muerto;
+            }
+        } else {
+            if(vector_acc < umbral*0.9 && !contar_pasos){
+                tiempo_muerto = !tiempo_muerto;
+            }
+        
+            if (vector_acc > umbral*1.1 && contar_pasos){// Controla si se dio un paso o no
+                //Levanta bandera para no seguir contando pasos
+                contar_pasos = !contar_pasos; 
 
-            //Se da formato y se envia msg por BT
-            sprintf(msg, "Cantidad de pasos: %d\n", cantidad_pasos );
-            BleSendString(msg);
+                cantidad_pasos++; //Suma un paso al detectar que el vector_acc supero el umbral y la bandera esta baja
 
-        } else if (vector_acc < umbral && !CONTAR_PASOS) {
-            CONTAR_PASOS = !CONTAR_PASOS; //Baja bandera
-        }        
+                //Se da formato y se envia msg por BT
+                sprintf(msg, "Cantidad de pasos: %d\n", cantidad_pasos );
 
+                BleSendString(msg);
+            }   
+        }
+        
         contador++;
-        if(contador >= 20) contador = 0; //Reinicia contador
-
+        if(contador >= ULTIMOS_VALORES) contador = 0; //Reinicia contador
 	}
 }
 
 /** @fn Calibrate_Acc
  * 	@brief Calibra el acelerometro calculando los valores promedios del estado inicial
- *  @param[in] No hay parámetros
  * 	@return 1 (TRUE) si no hay error
 */
 bool Calibrate_Acc(){
     /* Loop para leer los valores iniciales */
+    x_promedio = 0;
+    y_promedio = 0;
+    z_promedio = 0;
     for (uint16_t i = 0; i < LECTURAS_CALIBRACION; i++)
     {
         /* Sumatoria de los primeros *LECTURAS_CALIBRACION* valores */
@@ -195,6 +208,13 @@ bool Calibrate_Acc(){
     return 1;
 }
 
+void read_data(uint8_t * data, uint8_t length){
+    /* Data */
+    if(data[0] == 'R'){
+        cantidad_pasos = 0;
+        Calibrate_Acc();
+    } 
+}
 /*==================[external functions definition]==========================*/
 void app_main(void){
     /* Inicializacion de acelerometro */
@@ -230,6 +250,7 @@ void app_main(void){
 	/* Inicialización del conteo de timers */
 	TimerStart(timer_readacc.timer);
 
+    // While para usar LEDs de guia para la conexion BT
     while(1){
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
         switch(BleStatus()){
